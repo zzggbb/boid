a07d9e89496f43acd21cd7b0515af3f6465fd6e4
Zane Bradley <zbradley96@gmail.com>
2014-10-28 00:48:59 -0700

    refactored vector stuff

--- boids.js
+++ boids.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
+    
     cfg = {
-        'debug': true,
+        'debug': false,
+        'loop_interval': 10,
         'canvas': {
-            'width': 500,
-            'height': 500
+            'width': window.innerWidth / 2,
+            'height': window.innerHeight
         },
         'boid': {
+            'count': 100,
             'size': 10,
             'velocity_magnitude': 4
         },
         'avoid_distance': 30,
         'velocity_adjust': 8
     }
+
     $('body').append(
         $('<canvas>').attr({
             'id': 'canvas',
@@ -21,6 +25,7 @@ $(document).ready(function() {
     )
     
     ctx = $('#canvas')[0].getContext('2d')
+
     rand = {
         'integer': function(min, max) {
             return Math.floor(Math.random() * (max - min)) + min;
@@ -45,27 +50,27 @@ $(document).ready(function() {
         }
     }
     vector =  {
-        'zero': {'x':0,'y':0},
+        'make': function(x, y) {
+            return {
+                'x': x,
+                'y': y
+            }
+        }, 
+        'zero': function() { return this.make(0, 0) },
         'sum': function (vectors) {
-            return vectors.reduce(this.add, this.zero)
+            return vectors.reduce(this.add, this.zero())
         },
         'add': function(v1, v2) {
-            return {
-                'x': v1.x + v2.x,
-                'y': v1.y + v2.y
-            }
+            return this.make(v1.x + v2.x, v1.y + v2.y)
         },
         'diff': function(v1, v2) {
-            return {
-                'x': v1.x - v2.x,
-                'y': v1.y - v2.y
-            }
+            return this.make(v1.x - v2.x, v1.y - v2.y)
         },
         'quotient': function(v, s) {
-            return {
-                'x': v.x / s,
-                'y': v.y / s
-            }
+            return this.make(v.x / s, v.y / s)
+        },
+        'product': function(v, s) {
+            return this.make(v.x * s, v.y * s)
         },
         'mean': function(vectors) {
             resultant = this.sum(vectors)
@@ -83,42 +88,51 @@ $(document).ready(function() {
         } 
     }
     rules = {
-        'huddle': function(me,boids) {
-            unique = boids.filter(function(i) {
-                return !vector.equal(me.pos,i.pos)
-            })
-            vectors = boids.map(function(i) {
-                return i.pos
-            })
-            flock = vector.mean(vectors)
-            distance = vector.diff(flock, me.pos)
-            adjust = vector.quotient(distance,100)
-            return adjust
+        'huddle': {
+            'vector' : function(me,boids) {
+                unique = boids.filter(function(i) {
+                    return !vector.equal(me.pos,i.pos)
+                })
+                vectors = boids.map(function(i) {
+                    return i.pos
+                })
+                flock = vector.mean(vectors)
+                distance = vector.diff(flock, me.pos)
+                adjust = vector.quotient(distance,100)
+                return adjust
+            },
+            'weight': 1
         },
-        'avoid': function(me, boids) {
-            unique = boids.filter(function(i) {
-                return !vector.equal(me.pos,i.pos)
-            }) 
-            neighbors = unique.filter(function(i) { 
-                return vector.distance(me.pos,i.pos) < cfg.avoid_distance 
-            })
-            displacements = neighbors.map(function(i) {
-                return vector.diff(i.pos, me.pos)
-            })
-            adjust = vector.diff({'x':0,'y':0}, vector.sum(displacements))
-            return adjust
+        'avoid': {
+            'vector': function(me, boids) {
+                unique = boids.filter(function(i) {
+                    return !vector.equal(me.pos,i.pos)
+                }) 
+                neighbors = unique.filter(function(i) { 
+                    return vector.distance(me.pos,i.pos) < cfg.avoid_distance 
+                })
+                displacements = neighbors.map(function(i) {
+                    return vector.diff(i.pos, me.pos)
+                })
+                adjust = vector.diff({'x':0,'y':0}, vector.sum(displacements))
+                return adjust
+            },
+            'weight': 1
         },
-        'speed': function(me,boids) {
-            unique = boids.filter(function(i) {
-                return !vector.equal(me.vel, i.vel)
-            })
-            velocities = unique.map(function(i) {
-                return i.vel
-            })
-            pace = vector.mean(velocities)
-            adjust = vector.quotient(vector.diff(pace,me.vel), cfg.velocity_adjust)
-            cfg.debug && console.log(me,adjust)
-            return adjust
+        'speed': {
+            'vector': function(me,boids) {
+                unique = boids.filter(function(i) {
+                    return !vector.equal(me.vel, i.vel)
+                })
+                velocities = unique.map(function(i) {
+                    return i.vel
+                })
+                pace = vector.mean(velocities)
+                adjust = vector.quotient(vector.diff(pace,me.vel), cfg.velocity_adjust)
+                cfg.debug && console.log(me,adjust)
+                return adjust
+            },
+            'weight': 1
         }
     }
 
@@ -130,7 +144,7 @@ $(document).ready(function() {
                     'x': rand.integer(0,cfg.canvas.width),
                     'y': rand.integer(0,cfg.canvas.height),
                 },
-                // ensure non-zero velocity, in either direction
+                // ensure non-zero velocity
                 'vel': {
                     'x': rand.integer(1,cfg.boid.velocity_magnitude) * rand.choice([-1,1]),
                     'y': rand.integer(1,cfg.boid.velocity_magnitude) * rand.choice([-1,1])
@@ -140,15 +154,23 @@ $(document).ready(function() {
         'draw': function(boid) {
             ctx.fillStyle = boid.color
             ctx.fillRect(boid.pos.x,boid.pos.y,cfg.boid.size,cfg.boid.size)
+
+            ctx.fill();
+            //ctx.rotate(-1 * Math.atan2(boid.vel.y, boid.vel.x))
+            return boid
         },
         'move': function(boid) {
             /*
             resultant = vector.sum([
-                _.values(rules).map(function(f) {
+                .map(function(f) {
                     return f(boid,boids)
                 })
             ])*/
-            resultant = vector.zero
+            /* 
+                should be the sum of all `rules` applied to the current 
+                `boid` and its surrounding `boids`
+            */
+            resultant = vector.zero()
             boid.vel = vector.add(boid.vel, resultant)
             boid.pos = vector.add(boid.pos, boid.vel)
             if (boid.pos.x > cfg.canvas.width) {
@@ -163,27 +185,26 @@ $(document).ready(function() {
             if (boid.pos.y < 0) {
                 boid.pos.y = cfg.canvas.height
             }
-        }
-    }
-
-    boids = _.range(100).map(boid.make)
-
-    loop = function() {
-        //ctx.clearRect(0, 0, cfg.canvas.width, cfg.canvas.height)
-        cfg.canvas.height = window.innerHeight
-        cfg.canvas.width = window.innerWidth / 2
-        
-        ctx.canvas.height = cfg.canvas.height
-        ctx.canvas.width = cfg.canvas.width
-        //console.log(boids[0].pos)
-        boids.map(boid.draw)
-        boids.map(boid.move)
-
-    }
-    setInterval(
-        function(){
-            loop()
         },
-        10
-    ) 
+    }
+    boids = Array.apply(null,Array(cfg.boid.count)).map(boid.make)
+    
+    main = {
+        'loop': function() {
+            cfg.canvas.width = window.innerWidth / 2
+            cfg.canvas.height = window.innerHeight
+            
+            ctx.canvas.width = cfg.canvas.width
+            ctx.canvas.height = cfg.canvas.height
+            cfg.debug && console.log(boids[0].pos)
+            
+            boids.map(boid.draw).map(boid.move)
+        },
+        'init': function() {
+            setInterval(this.loop, cfg.loop_interval)
+        }
+        
+    }
+    main.init()
+
 })
